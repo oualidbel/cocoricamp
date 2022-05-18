cad2fd34da890d9b64052d5c0b3522021762395f
Dorian Cottin <cottin.dotian@gmail.com>
2022-05-18 17:38:28 +0200

    v6 Dorian

--- src/Form/RegistrationType.php
+++ src/Form/RegistrationType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
@@ -15,14 +16,29 @@ class RegistrationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-        // fisrtname and lastname
-            ->add('firstname')
-            ->add('lastname')
+            ->add('firstname', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Prénom'
+                ],
+                'required' => true
+            ])
+            ->add('lastname', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Nom'
+                ],
+                'required' => true
+            ])
             ->add('email', EmailType::class, [
-                'required' => false
+                'attr' => [
+                    'placeholder' => 'Email'
+                ],
+                'required' => true
             ])
             ->add('password', PasswordType::class, [
-                'required' => false
+                'attr' => [
+                    'placeholder' => 'Mot de passe'
+                ],
+                'required' => true
             ])
             ->add('submit', SubmitType::class, [
                 'label' => "S'inscrire"
