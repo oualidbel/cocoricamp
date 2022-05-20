0a759d6e415f71744cb1b7a499b7ac461552b507
Dorian Cottin <cottin.dotian@gmail.com>
2022-05-20 11:38:40 +0200

    v9 Dorian

--- public/js/nav.js
+++ public/js/nav.js
@@ -1,5 +1,30 @@
 const mobileNavMenu = document.querySelector('.mobile-nav-menu');
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const profilNavMenu = document.querySelector('.profil-nav-menu');
 
 function mobileNavMenuFunction() {
     mobileNavMenu.classList.toggle('active');
+    mobileNavButton.classList.toggle('button-active');
+    if (profilNavMenu.classList.contains('active')) {
+        profilNavMenu.classList.remove('active');
+    }
 }
+
+function profilNavMenuFunction() {
+    profilNavMenu.classList.toggle('active');
+    if (mobileNavMenu.classList.contains('active')) {
+        mobileNavMenu.classList.remove('active');
+        mobileNavButton.classList.remove('button-active');
+    }
+}
+
+document.addEventListener('click', function(e) {
+    if (e.target.closest(".profil-nav-menu")) return;
+    if (e.target.closest(".profil-nav-button")) return;
+	profilNavMenu.classList.remove("active");
+    
+    if (e.target.closest(".mobile-nav-menu")) return;
+    if (e.target.closest(".mobile-nav-button")) return;
+	mobileNavMenu.classList.remove("active");
+    mobileNavButton.classList.remove("button-active");
+});
