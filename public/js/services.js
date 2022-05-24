dcd0b01ed0f0a69037663bba593a98114186a2f4
Dorian Cottin <cottin.dotian@gmail.com>
2022-05-24 16:35:12 +0200

    v15 Dorian

--- public/js/services.js
+++ public/js/services.js
@@ -1,19 +1,17 @@
-let service = document.querySelector('.services');
-let image = document.querySelector('.services img');
+let image = document.querySelector('.services-img');
 
 let counter = 1;
 
 
-console.log(service, image);
+console.log('test');
 
 // Modifie / manipule les éléments
 
-service.addEventListener('click', function() {
-    if (counter ==5) {
+image.addEventListener('click', function() {
+    if (counter == 5) {
         counter = 0;
     }
     counter++;
-   
-    image.src = 'public/images/services' + counter + '.jpg';
-
+    image.style.backgroundImage = `url(../images/services${counter}.jpg)`;
+    console.log(image.style.backgroundImage = `url(../images/services${counter}.jpg)`);
 });
