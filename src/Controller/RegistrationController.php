b2b899f5bdaf3a7a5fa5f974c258c486553ba1c3
Dorian Cottin <cottin.dotian@gmail.com>
2022-05-24 12:58:41 +0200

    v14 Dorian

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -16,7 +16,7 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 
 class RegistrationController extends AbstractController
 {
-    #[Route('/register', name: 'app_register')]
+    #[Route('/s\'inscrire', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
     {
         $user = new User();
