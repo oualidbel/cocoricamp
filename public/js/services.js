e09b5f57d846945f34d4d75c6a8a30391046d700
Dorian Cottin <cottin.dotian@gmail.com>
2022-05-27 11:14:11 +0200

    v19 Dorian

--- public/js/services.js
+++ public/js/services.js
@@ -1,17 +1,37 @@
 let image = document.querySelector('.services-img');
+const servicesContentItems = document.querySelectorAll('.services-content-item');
 
-let counter = 1;
-
-
-console.log('test');
+let counter = 0;
 
 // Modifie / manipule les éléments
 
-image.addEventListener('click', function() {
-    if (counter == 5) {
-        counter = 0;
-    } 
-    counter++;
-    image.style.backgroundImage = `url(../images/services${counter}.jpg)`;
-    console.log(image.style.backgroundImage = `url(../images/services${counter}.jpg)`);
-});
+function changeImageNext() {
+    if (counter == 3) {
+        counter = -1;
+        counter++;
+        image.style.backgroundImage = `url(../images/services${counter}.jpg)`;
+        servicesContentItems[3].classList.remove('active');
+        servicesContentItems[0].classList.add('active');
+    } else {
+        counter++;
+        image.style.backgroundImage = `url(../images/services${counter}.jpg)`;
+        servicesContentItems[counter-1].classList.remove('active');
+        servicesContentItems[counter].classList.add('active');
+    }
+};
+
+
+function changeImagePrevious() {
+    if (counter == 0) {
+        counter = 4;
+        counter--;
+        image.style.backgroundImage = `url(../images/services${counter}.jpg)`;
+        servicesContentItems[0].classList.remove('active');
+        servicesContentItems[3].classList.add('active');
+    } else {
+        counter--;
+        image.style.backgroundImage = `url(../images/services${counter}.jpg)`;
+        servicesContentItems[counter+1].classList.remove('active');
+        servicesContentItems[counter].classList.add('active');
+    }
+};
